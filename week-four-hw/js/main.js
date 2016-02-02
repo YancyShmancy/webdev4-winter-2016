473b623ba92e53d6dc42ea244954a22955fb56da
Zoey Yancy <yancyshmancy@gmail.com>
2016-02-02 17:06:47 -0500

    Finishing site

--- week-four-hw/js/main.js
+++ week-four-hw/js/main.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
 	
 	$(window).on("scroll", function(e) {
-
-		console.log( $(window).scrollTop() );
         
         var stickyTop = $('#navigation').offset().top;
         
@@ -15,5 +13,24 @@ $(document).ready(function(){
             $('#navigation.fixed').removeClass('fixed');
             $('#big-header').removeClass('scrolled');
         }
-	})
+	});
+    
+    $('.nav').onePageNav({
+        currentClass: 'active',
+        changeHash: false,
+        scrollSpeed: 750,
+        scrollThreshold: 0.5,
+        filter: '',
+        easing: 'swing',
+        begin: function() {
+            //I get fired when the animation is starting
+        },
+        end: function() {
+            //I get fired when the animation is ending
+        },
+        scrollChange: function($currentListItem) {
+            //I get fired when you enter a section and I pass the list item of the section
+        }
+    });
 });
+
