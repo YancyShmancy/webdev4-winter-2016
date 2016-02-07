21c160184394e37ce75f3d8e44fca37a5d28ab20
Zoey Yancy <yancyshmancy@gmail.com>
2016-02-07 00:20:01 -0500

    Starting Week Five Project

---
+++ week-five/assets/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#menu-toggle').on('click', function(){
+        $('#sidebar-wrapper').toggleClass('toggled');
+        $('#wrapper').toggleClass('toggled');
+    })
+})
