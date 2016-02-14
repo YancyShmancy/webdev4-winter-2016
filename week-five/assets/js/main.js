f38fc97192db1263543459a2bde3d04f08d3089e
Zoey Yancy <yancyshmancy@gmail.com>
2016-02-14 04:14:35 -0500

    Styling sub-pages

--- week-five/assets/js/main.js
+++ week-five/assets/js/main.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
     $('#menu-toggle').on('click', function(){
         $('#sidebar-wrapper').toggleClass('toggled');
         $('#wrapper').toggleClass('toggled');
+        $('#menu-toggle').toggleClass('toggled');
     });
 });
+
