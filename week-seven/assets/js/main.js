79e20b30fb43a86ab1e2c10fcae4e5a15675cc6c
Zoey Yancy <yancyshmancy@gmail.com>
2016-02-24 02:14:02 -0500

    Added tabs, began reservation form

--- week-seven/assets/js/main.js
+++ week-seven/assets/js/main.js
@@ -2,11 +2,14 @@ $(document).ready(function() {
     
     $('#explore-extend').on('click', function(){
         $('#explore-extra').toggleClass("hidden");
-    })
+    });
     
     $('a[href*=#]').click(function(e){
-        console.log("SMOOTH BEGIN");
         e.preventDefault();
+    });
+    
+    $('a[href*=#]').not('.mdl-tabs__tab').click(function(e){
+        console.log("SMOOTH BEGIN");
         var speed = 1000;
         var href= $(this).attr("href");
         var target = $(href == "#" || href == "" ? 'html' : href);
