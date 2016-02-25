88ad5d9f629ebadaefc1357df9aabc3cfd973327
Zoey Yancy <yancyshmancy@gmail.com>
2016-02-24 20:43:58 -0500

    Added sections and footer

--- week-seven/assets/js/main.js
+++ week-seven/assets/js/main.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
     
     $('a[href*=#]').not('.mdl-tabs__tab').click(function(e){
         console.log("SMOOTH BEGIN");
+        e.preventDefault();
         var speed = 1000;
         var href= $(this).attr("href");
         var target = $(href == "#" || href == "" ? 'html' : href);
@@ -17,5 +18,28 @@ $(document).ready(function() {
         $(".mdl-layout__content").animate({scrollTop:position}, speed, "swing");
         console.log("SMOOTH END");
     });
+    
+    var temp = "<div class='cell mdl-shadow--2dp' style='width:{width}px; height: {height}px; background-image: url(./assets/img/gallery-{index}.jpg)'></div>";
+    var w = 1, html = '', limitItem = 20;
+    for (var i = 0; i < limitItem; ++i) {
+        w = 200 +  200 * Math.random() << 0;
+        html += temp.replace(/\{height\}/g, 200).replace(/\{width\}/g, w).replace("{index}", i + 1);
+    }
+    $("#freewall").html(html);
 
+    var wall = new Freewall("#freewall");
+    wall.reset({
+        selector: '.cell',
+        animate: true,
+        cellW: 20,
+        cellH: 200,
+        gutterY: 5,
+        gutterX: 5,
+        onResize: function() {
+            wall.fitWidth();
+        }
+    });
+    wall.fitWidth();
+    // for scroll bar appear;
+    $(window).trigger("resize");
 })
